fa04beda2f33f462f9c28b5e48a86d2d051816ba
Adrien RUELLOU <aruellou@e1r5p13.42.fr>
2018-09-18 19:07:15 +0200

    [add] admin upload and delete

---
+++ admin/index.php
@@ -0,0 +1,102 @@
+<?php
+require "../functions.php";
+
+// print_r($_POST);
+// print_r($_FILES);
+
+if (!empty($_POST)) {
+    switch ($_POST['submit']) {
+        case 'Upload':
+            if (empty($_POST['project'])) {
+                echo 'Project empty';
+            } else if (empty($_POST['description'])) {
+                echo 'Description empty';
+            } else if (empty($_FILES['media'])) {
+                echo 'Media empty';
+            } else {
+                create_project($_POST, $_FILES);
+            }
+            break;
+
+        case 'Delete':
+            if (empty($_POST['project'])) {
+                echo 'No Project Specified';
+            } else {
+                delete_project($_POST['project']);
+            }
+            break;
+
+        default:
+            echo 'Unknown Submit Type';
+            break;
+    }
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Admin - CowbowFilms</title>
+    </head>
+    <body>
+        <form action="" method="post" enctype="multipart/form-data">
+            <label for="project">Nom du projet:</label>
+            <input type="text" name="project" value="" />
+            <br>
+            <label for="description">Text:</label>
+            <input type="text" name="description" value="">
+            <br>
+            <label for="media1" id="mediaLabel">Media1</label>
+            <input type="file"
+                id="media1" name="media[]"
+                accept="image/*, video/*" />
+            <br>
+
+            <input type="submit" name="submit" value="Envoyer">
+        </form>
+        <button onClick="addMediaInput()">+</button>
+        <form action="" method="post">
+            <select name="project">
+                <?php
+                foreach (get_project() as $key => $value) {
+                    echo "<option value='$value'>$value</option>";
+                }
+                ?>
+            </select>
+            <input type="submit" name="submit" value="Delete">
+        </form>
+
+        <div>
+            <?php
+            foreach (get_project() as $key => $value) {
+                echo '<br><br>';
+                echo $value;
+                echo '<br>';
+                foreach (get_files($value) as $key => $value) {
+                    echo "<li>$value</li>";
+                }
+                // print_r(get_files($value));
+            }
+            ?>
+        </div>
+    </body>
+</html>
+
+<script>
+    index = 1
+
+    function addMediaInput() {
+        input = document.getElementById('media1').cloneNode()
+        media = document.getElementById('media'+index)
+        label = document.getElementById('mediaLabel').cloneNode()
+        index++
+
+        input.setAttribute('id', 'media'+index)
+        label.setAttribute('id', 'mediaLabel'+index)
+        label.innerText = "Media"+index
+
+        media.after(document.createElement('br'), label, input)
+    }
+</script>
---
+++ functions.php
@@ -0,0 +1,92 @@
+<?php
+function get_project()
+{
+    $project = scandir(__DIR__ . "/__PROJECTS");
+    array_splice($project, 0, 2);
+    return $project;
+}
+
+function get_files($project)
+{
+    $files = scandir(__DIR__ . "/__PROJECTS/" . $project);
+    array_splice($files, 0, 2);
+    return $files;
+}
+
+function get_text($project)
+{
+    return file_get_contents(__DIR__ . "/__PROJECTS/" . $project . "/text.txt");
+}
+
+function set_text($project, $text)
+{
+    file_put_contents(__DIR__ . "/__PROJECTS/" . $project . "/text.txt", $text);
+}
+
+function get_index()
+{
+    return file_get_contents(__DIR__ . "/__INDEX.txt");
+}
+
+function set_index($text)
+{
+    file_put_contents(__DIR__ . "/__INDEX.txt", $text);
+}
+
+function get_files_path($project)
+{
+    $splice = [];
+    $files = get_files($project);
+    foreach ($files as $key => $value) {
+        if (preg_match("/thumbnail/", $value)) {
+            array_push($splice, $key);
+        }
+
+        $files[$key] = "/__PROJECTS/" . $project . "/" . $files[$key];
+    }
+    foreach ($splice as $key => $value) {
+        array_splice($files, $value - $key, 1);
+    }
+    return $files;
+}
+
+function create_folders($name)
+{
+    mkdir(__DIR__ . "/__PROJECTS/" . $name);
+}
+
+function upload_media($project, $files)
+{
+    foreach ($files["media"]["error"] as $key => $error) {
+        if ($key <= 9 && $key >= 0) {
+            $index = '0' . $key;
+        }
+        if ($error == UPLOAD_ERR_OK) {
+            $tmp_name = $files["media"]["tmp_name"][$key];
+            $name = $index . '.' . end(split('/', $files["media"]["type"][$key]));
+            move_uploaded_file($tmp_name, __DIR__ . "/__PROJECTS/" . $project . '/' . $name);
+        }
+    }
+}
+
+function removeDirectory($path)
+{
+    $files = glob($path . '/*');
+    foreach ($files as $file) {
+        is_dir($file) ? removeDirectory($file) : unlink($file);
+    }
+    rmdir($path);
+    return;
+}
+
+function delete_project($project)
+{
+    removeDirectory(__DIR__ . "/__PROJECTS/" . $project);
+}
+
+function create_project($post, $files)
+{
+    create_folders($post['project']);
+    set_text($post['project'], $post['description']);
+    upload_media($post['project'], $files);
+}
